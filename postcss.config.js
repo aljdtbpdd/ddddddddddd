f774e85057158c78c4ee84b29c9cdf725946451e
lp <1474760202@qq.com>
2019-08-09 21:08:39 +0800

    配置ref

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,21 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {}
+const {
+  sep
+} = require('path')
+//    a/b/c
+module.exports = ({
+  file
+}) => {
+  let rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
+    }
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,19 @@
   <div id="app">
 
     <router-view/>
+    <van-button type="default">默认按钮</van-button>
+<van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+<div class="box"></div>
   </div>
 </template>
 
 <style lang="less">
-
+ .box{
+   width: 750px;
+   height: 60px;
+   background-color: pink;
+ }
 </style>
